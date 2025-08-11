34481c707460cc0f6fd3d533e024306d5396755b
Farid Zelli <82771409+FaridZelli@users.noreply.github.com>
2025-08-11 20:36:50 +0330

    Add files via upload

--- shadowsocks_deploy.sh
+++ shadowsocks_deploy.sh
@@ -23,8 +23,16 @@ fi
 # Uninstall section
 if [[ "$1" == "-u" ]]; then
 echo -e "
-This script will reconfigure your environment and install shadowsocks on your server.
-I am not responsible for any damages or data loss that may occur.
+https://github.com/FaridZelli/ShadowsocksQuickDeployment
+
+This script installs Shadowsocks on your \033[33mUbuntu 24.04\033[0m server.
+
+By typing Y and pressing Enter, you agree that the author
+bears no liability for any loss, damage or data corruption,
+whether direct, indirect, incidental, consequential or punitive,
+that may result from its use.
+
+Continued use assumes your acceptance of these terms.
 
 \033[33mAre you sure you want to remove all files previously created by this script? (Y/N)\033[0m
 "
@@ -34,7 +42,10 @@ read -p "Your choice:" ANSWER
 case $ANSWER in
   [Yy]* )
     # Proceed to uninstall
-    echo "Uninstalling..."
+    echo -e "
+--------------------------------------------------
+\033[31mUninstalling...\033[0m
+--------------------------------------------------"
     systemctl stop shadowsocks.service
     systemctl disable shadowsocks.service
     rm -rf ~/shadowsocks-tmp
@@ -50,7 +61,10 @@ case $ANSWER in
     rm -rf /usr/local/bin/ssserver
     rm -rf /usr/local/bin/ssservice
     rm -rf /usr/local/bin/ssurl
-    echo "Done!"
+    echo -e "
+--------------------------------------------------
+Done!
+--------------------------------------------------"
     exit 1
     ;;
   * )
@@ -63,8 +77,16 @@ fi
 
 # Ask whether to proceed
 echo -e "
-This script will reconfigure your environment and install shadowsocks on your server.
-I am not responsible for any damages or data loss that may occur.
+https://github.com/FaridZelli/ShadowsocksQuickDeployment
+
+This script installs Shadowsocks on your \033[33mUbuntu 24.04\033[0m server.
+
+By typing Y and pressing Enter, you agree that the author
+bears no liability for any loss, damage or data corruption,
+whether direct, indirect, incidental, consequential or punitive,
+that may result from its use.
+
+Continued use assumes your acceptance of these terms.
 
 \033[33mDo you wish to continue? (Y/N)\033[0m
 "
@@ -74,6 +96,10 @@ read -p "Your choice:" ANSWER
 case $ANSWER in
   [Yy]* )
     # Proceed with the rest of the script
+    echo -e "
+--------------------------------------------------
+Starting...
+--------------------------------------------------"
     ;;
   * )
     # Stop the script for any other input
@@ -83,47 +109,12 @@ case $ANSWER in
 esac
 
 # -----
-# get shadowsocks
-# -----
-
-LATEST_VERSION=$(curl -s https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest | grep -Po '"tag_name":\s*"\K.*?(?=")')
-
-if [ -z "$LATEST_VERSION" ]; then
-    echo "Could not get the latest version of shadowsocks-rust. Stopping the script..."
-    exit 1
-fi
-
-mkdir -p ~/shadowsocks-tmp
-mkdir -p ~/shadowsocks-tmp/bin
-
-cd ~/shadowsocks-tmp
-
-curl -s https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest \
-  | grep -Po '"tag_name":\s*"\K.*?(?=")' \
-  | xargs -I % curl -L -O https://github.com/shadowsocks/shadowsocks-rust/releases/download/%/shadowsocks-%.x86_64-unknown-linux-gnu.tar.xz
-
-tar -xf shadowsocks-*.x86_64-unknown-linux-gnu.tar.xz -C ~/shadowsocks-tmp/bin
-
-mv ~/shadowsocks-tmp/bin/* /usr/local/bin/
-
-rm -rf ~/shadowsocks-tmp
-
-echo "Installed Shadowsocks to /usr/local/bin"
-
-# -----
 # sshd_config
 # -----
 
 echo -e "
-\033[33mWould you like to disable SSH password authentication? (Recommended)\033[0m
-
---------------------------------------------------
-PubkeyAuthentication yes
-PasswordAuthentication no
-ChallengeResponseAuthentication no
-UsePAM no
-PermitEmptyPasswords no
---------------------------------------------------
+\033[33mWould you like to disable SSH password authentication?
+This step is only recommended if you have already setup your public key.\033[0m
 
 1) Yes, disable SSH password authentication
 2) No, skip this step
@@ -133,7 +124,7 @@ PermitEmptyPasswords no
 read -p "Your choice:" ANSWER
 # Read input
 case $ANSWER in
-  1 ) 
+  1 )
 mkdir -p /etc/ssh/sshd_config.d
 if [ ! -e /etc/ssh/sshd_config.d/01-ssh-hardening.conf ]; then
   cat <<EOF > /etc/ssh/sshd_config.d/01-ssh-hardening.conf
@@ -143,16 +134,18 @@ ChallengeResponseAuthentication no
 UsePAM no
 PermitEmptyPasswords no
 EOF
-  echo "Created /etc/ssh/sshd_config.d/01-ssh-hardening.conf"
+  echo "
+  Created /etc/ssh/sshd_config.d/01-ssh-hardening.conf"
 else
-  echo "/etc/ssh/sshd_config.d/01-ssh-hardening.conf already exists! Skipping..."
+  echo "
+  /etc/ssh/sshd_config.d/01-ssh-hardening.conf already exists! Skipping..."
 fi
     ;;
-  2 ) 
+  2 )
     # Proceed with the rest of the script
     echo "Skipping..."
     ;;
-  0 ) 
+  0 )
     # Exit the script
     echo "Stopping the script..."
     exit 1
@@ -163,6 +156,133 @@ fi
     exit 1
     ;;
 esac
+
+# -----
+# detect environment
+# -----
+
+# Function to detect system architecture
+detect_arch() {
+    local machine=$(uname -m)
+    case "$machine" in
+        aarch64|arm64) ARCH="aarch64" ;;
+        arm) ARCH="arm" ;;
+        armv7l) ARCH="armv7" ;;
+        i686|x86) ARCH="i686" ;;
+        loongarch64) ARCH="loongarch64" ;;
+        mips) ARCH="mips" ;;
+        mips64el) ARCH="mips64el" ;;
+        mipsel) ARCH="mipsel" ;;
+        riscv64) ARCH="riscv64gc" ;;
+        x86_64) ARCH="x86_64" ;;
+        *) ARCH="unknown" ;;
+    esac
+}
+
+# Function to detect C library type
+detect_libc() {
+    # Check for musl first
+    if ldd --version 2>&1 | grep -q "musl"; then
+        # Determine specific musl variant
+        local libc=$(ldd --version 2>&1)
+        if [[ "$libc" == *"musleabi"* ]]; then
+            C_LIBRARY="musleabi"
+        elif [[ "$libc" == *"musleabihf"* ]]; then
+            C_LIBRARY="musleabihf"
+        else
+            C_LIBRARY="musl"
+        fi
+    else
+        # If not musl, assume GNU
+        local libc=$(ldd --version 2>&1)
+        if [[ "$libc" == *"gnueabi"* ]]; then
+            C_LIBRARY="gnueabi"
+        elif [[ "$libc" == *"gnueabihf"* ]]; then
+            C_LIBRARY="gnueabihf"
+        elif [[ "$libc" == *"gnuabi64"* ]]; then
+            C_LIBRARY="gnuabi64"
+        else
+            C_LIBRARY="gnu"
+        fi
+    fi
+}
+
+# Detect architecture and C library
+detect_arch
+detect_libc
+
+# Print detected values (optional)
+echo "
+Detected System Architecture: $ARCH
+Detected System C Library: $C_LIBRARY"
+
+# -----
+# get shadowsocks
+# -----
+
+LATEST_VERSION=$(curl -s https://api.github.com/repos/shadowsocks/shadowsocks-rust/releases/latest | grep -Po '"tag_name":\s*"\K.*?(?=")')
+
+download_failed() {
+    read -p "
+We are unable to download the latest shadowsocks-rust release from GitHub at this time.
+If you have already downloaded Shadowsocks binaries locally, please enter the directory in which they are stored: " dir
+
+    # Check if directory exists
+    if [[ ! -d "$dir" ]]; then
+      echo "Error: Directory '$dir' does not exist, stopping the script..."
+      exit 1
+    fi
+
+    # Get list of files
+    files=("$dir"/*)
+    if [[ ${#files[@]} -eq 0 ]]; then
+      echo "Error: Directory '$dir' is empty, stopping the script..."
+      exit 1
+    fi
+
+    echo "Select a file from the list below:"
+    echo "0) Exit"
+    index=1
+    for filepath in "${files[@]}"; do
+      echo "$index) $(basename "$filepath")"
+      ((index++))
+    done
+
+    read -p "Enter your choice [0-${#files[@]}]: " choice
+
+    if [[ "$choice" -eq 0 ]]; then
+      echo "No archive selected, stopping the script..."
+      exit 0
+    fi
+
+    if ! [[ "$choice" =~ ^[0-9]+$ ]] || (( choice < 0 || choice > ${#files[@]} )); then
+      echo "Invalid choice, stopping the script..."
+      exit 1
+    fi
+
+    # Extract selected archive
+    SHADOWSOCKS_LOCAL_ARCHIVE="${files[$((choice-1))]}"
+    echo "
+Selected archive: $SHADOWSOCKS_LOCAL_ARCHIVE"
+    tar -xf $SHADOWSOCKS_LOCAL_ARCHIVE -C /usr/local/bin
+    echo "
+Installed Shadowsocks to /usr/local/bin"
+}
+
+if [ -z "$LATEST_VERSION" ]; then
+    download_failed
+else
+    echo "Latest Shadowsocks release: $LATEST_VERSION
+    "
+    mkdir -p ~/shadowsocks-tmp
+    curl -Lo ~/shadowsocks-tmp/shadowsocks-bin.tar.xz \
+    https://github.com/shadowsocks/shadowsocks-rust/releases/download/${LATEST_VERSION}/shadowsocks-${LATEST_VERSION}.${ARCH}-unknown-linux-${C_LIBRARY}.tar.xz \
+    && tar -xf ~/shadowsocks-tmp/shadowsocks-bin.tar.xz -C /usr/local/bin \
+    || download_failed
+    rm -rf ~/shadowsocks-tmp
+    echo "
+Installed Shadowsocks to /usr/local/bin"
+fi
 
 # -----
 # sysctl
@@ -218,9 +338,11 @@ net.ipv4.tcp_congestion_control = hybla
 # for low-latency network, use cubic instead
 # net.ipv4.tcp_congestion_control = cubic
 EOF
-  echo "Created /etc/sysctl.d/01-shadowsocks-optimizer.conf"
+  echo "
+Created /etc/sysctl.d/01-shadowsocks-optimizer.conf"
 else
-  echo "/etc/sysctl.d/01-shadowsocks-optimizer.conf already exists! Skipping..."
+  echo "
+/etc/sysctl.d/01-shadowsocks-optimizer.conf already exists! Skipping..."
 fi
 
 sysctl --system
@@ -247,9 +369,11 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOF
-  echo "Created /etc/systemd/system/shadowsocks.service"
+  echo "
+Created /etc/systemd/system/shadowsocks.service"
 else
-  echo "/etc/systemd/system/shadowsocks.service already exists! Skipping..."
+  echo "
+/etc/systemd/system/shadowsocks.service already exists! Skipping..."
 fi
 
 # -----
@@ -257,6 +381,9 @@ fi
 # -----
 
 setcap 'CAP_NET_BIND_SERVICE+ep' /usr/local/bin/ssserver
+
+echo "
+Granted ssserver permission to bind to low-numbered (privileged) network ports."
 
 # this is so the non-root user can access ports below 1024
 # routing perms can be acquired through 'CAP_NET_BIND_SERVICE,CAP_NET_ADMIN+ep'
@@ -266,14 +393,17 @@ setcap 'CAP_NET_BIND_SERVICE+ep' /usr/local/bin/ssserver
 # -----
 
 if id "ssuser" &>/dev/null; then
-  echo "User 'ssuser' already exists! Skipping..."
+  echo "
+User 'ssuser' already exists! Skipping..."
 else
   useradd -m "ssuser"
-  echo "User 'ssuser' created."
+  echo "
+User 'ssuser' created."
 fi
 
 # Prompt for and set the user's password
-echo "Please enter a password for 'ssuser':"
+echo "
+Please enter a password for 'ssuser':"
 passwd "ssuser"
 
 # -----
@@ -296,9 +426,11 @@ if [ ! -e /home/ssuser/shadowsocks-server.json ]; then
   // "keep_alive": 60
 }
 EOF
-  echo "Created /home/ssuser/shadowsocks-server.json"
+  echo "
+Created /home/ssuser/shadowsocks-server.json"
 else
-  echo "/home/ssuser/shadowsocks-server.json Opening..."
+  echo "
+Opening /home/ssuser/shadowsocks-server.json"
 fi
 
 nano /home/ssuser/shadowsocks-server.json
@@ -306,9 +438,13 @@ nano /home/ssuser/shadowsocks-server.json
 systemctl daemon-reload
 systemctl enable shadowsocks.service
 
+echo "
+Started shadowsocks.service"
+
 # End of script
 echo -e "
 --------------------------------------------------
 \033[32mIt's time to reboot!\033[0m
-Shadowsocks configuration location: /home/ssuser/shadowsocks-server.json
+Your Shadowsocks configuration is located at:
+/home/ssuser/shadowsocks-server.json
 --------------------------------------------------"
